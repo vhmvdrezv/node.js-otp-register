60136f505f0e4e2e7ad16b6d6d632e0953b57a58
vhmvdrezv <ahmdrzashn@gmail.com>
2024-10-24 18:01:05 +0330

    first commit

--- models/User.js
+++ models/User.js
@@ -2,6 +2,11 @@ import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
     name: String,
+    email: {
+        type: String,
+        unique: true,
+        required: false
+    },
     username: {
         type: String, 
         required: true,
